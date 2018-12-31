9d2654665485fa0ab573e55fb16943145a60f968
kasheeram <kashee.nitd2016@gmail.com>
2018-12-31 10:51:26 +0530

    Initial Commit

---
+++ WalkthroughAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WalkthroughAnimations
+//
+//  Created by kashee on 31/12/18.
+//  Copyright © 2018 kashee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
